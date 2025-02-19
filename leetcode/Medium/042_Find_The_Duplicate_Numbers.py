b35dad87143f868e4b9512eee764517fe3f691ef
ynxaron <ynx.aron@outlook.com>
2025-02-19 09:24:47 +0530

    Adding comments (explanation) to Medium/042_Find_The_Duplicate_Numbers.py

--- leetcode/Medium/042_Find_The_Duplicate_Numbers.py
+++ leetcode/Medium/042_Find_The_Duplicate_Numbers.py
@@ -1,3 +1,7 @@
+# The crux of this algorithm is to exploit the fact that since
+# there would only be one element among [1..(n-1)] that would be
+# repeated, if we take the sum of nums, and sum of (1..(n-1)), and
+# subtract the former from the latter, we would have our solution
 def find_the_duplicate_numbers(nums: list[int]) -> int:
     num_len = len(nums) - 1
     num_sum = (num_len * (num_len + 1)) // 2
